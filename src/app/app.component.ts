c2fb034b56d01b9efc75a65ebc0f589579e5223a
Matteo Finistauri <matteo_finistauri@hotmail.com>
2018-07-05 17:36:23 +0200

    Committed the entire project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {App} from "botframework-webchat";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div id="bot-chat-container" #botWindow></div>`
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild("botWindow") botWindowElement: ElementRef;
+  direct_line_secret: string = 'VHHXtjk5RzQ.cwA.5WM.o8BCHZhssuCgcNyT3uM1Z7HCrmAaAz1rk-6G8xMgieE';
+
+  ngOnInit(): void {
+    App({
+      directLine: {secret: this.direct_line_secret },
+      user: {id: 'user'},
+      bot: {id: 'bot'},
+      adaptiveCardsHostConfig: {}
+    }, this.botWindowElement.nativeElement)
+  }
 }
